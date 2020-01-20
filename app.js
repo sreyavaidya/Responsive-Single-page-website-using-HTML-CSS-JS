a0712c647b66c69b1bbb1ac1b60ec17882f0af8c
Shreya Vaidya <52731702+sreyavaidya@users.noreply.github.com>
2020-01-20 22:18:59 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -20,4 +20,19 @@ const navSlide = () =>{
 }
 
 navSlide();
+//scrollmagic
+function splitScroll(){
+    const controller = new ScrollMagic.Controller();
+
+    new ScrollMagic.Scene({
+        duration: '200%',
+        triggerElement: '.about-title',
+        triggerHook: 0
+    })
+    .setPin('.about-title')
+    .addIndicators()
+    .addTo(controller);
+}
+
+splitScroll();
 
